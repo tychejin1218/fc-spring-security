faaf850fffd86f3923377e9b43f600665715b97d
너나들이 <tychejin@naver.com>
2025-02-04 14:14:46 +0900

    docs(fcss-10): RequestMatchers

--- fcss-10/src/main/java/fast/campus/fcss10/config/SecurityConfiguration.java
+++ fcss-10/src/main/java/fast/campus/fcss10/config/SecurityConfiguration.java
@@ -16,49 +16,64 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
-        UserDetails danny = User.withUsername("danny.kim")
-                .password("12345")
-                .roles("ADMIN")
-                .build();
+  @Bean
+  public UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
-        UserDetails steve = User.withUsername("steve.kim")
-                .password("12345")
-                .roles("MANAGER")
-                .build();
+    UserDetails danny = User.withUsername("danny.kim")
+        .password("12345")
+        .roles("ADMIN")
+        .build();
 
-        manager.createUser(danny);
-        manager.createUser(steve);
+    UserDetails steve = User.withUsername("steve.kim")
+        .password("12345")
+        .roles("MANAGER")
+        .build();
 
-        return manager;
-    }
+    manager.createUser(danny);
+    manager.createUser(steve);
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+    return manager;
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic(Customizer.withDefaults());
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
 
-//        httpSecurity.authorizeHttpRequests(c -> c.requestMatchers("/product/{code:^[0-9]*$}")
-//                .permitAll());
-//        httpSecurity.authorizeHttpRequests(c -> c.anyRequest().denyAll());
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.httpBasic(Customizer.withDefaults());
 
-        httpSecurity.authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.GET, "/api/v1/hello")
-                .authenticated()); // HTTP GET 방식으로 /api/v1/hello 를 요청하면 앱이 사용자를 인증해야 함
-        httpSecurity.authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.POST, "/api/v1/hello")
-                .permitAll()); // HTTP POST 방식으로 /api/v1/hello 를 요청하면 모두 허용함
-        httpSecurity.authorizeHttpRequests(c -> c.requestMatchers("/api/v1/hello/**")
-                .authenticated()); // /api/v1/hello/** 식은 접두사 /api/v1/hello 가 붙은 모든 경로를 의미함
-        httpSecurity.authorizeHttpRequests(c -> c.anyRequest()
-                .denyAll()); // 나머지 요청은 모두 거부함
+    // 엔드포인트의 PathVariable 에 숫자만 받도록 하기 위해서는?
+    // @Valid 를 사용해도 되지만..
+    // 스프링 시큐리티를 적극 활용한다면?
+    // 문자가 하나라도 포함되어 있으면 401 Unauthorized, 숫자만 있으면 200 OK 와 함께 해당 숫자 반환
+    /*httpSecurity.authorizeHttpRequests(c -> c.requestMatchers("/product/{code:^[0-9]*$}")
+        .permitAll());
+    httpSecurity.authorizeHttpRequests(c -> c.anyRequest().denyAll());*/
 
-        httpSecurity.csrf(AbstractHttpConfigurer::disable); // HTTP POST 방식으로 /api/h1/hello 를 호출할 수 있도록 CSRF 비활성화
-        return httpSecurity.build();
-    }
+    // HTTP 방식에 따라 다른 권한 적용
+    // GET /api/v1/hello 에는 인증을 요구
+    // 인증이 없으면 401 Unauthorized, 인증이 있으면 200 OK
+    httpSecurity.authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.GET, "/api/v1/hello")
+        .authenticated());
+
+    // POST /api/v1/hello 에는 인증 없이 허용
+    // 인증이 있어도 없어도 200 OK
+    httpSecurity.authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.POST, "/api/v1/hello")
+        .permitAll());
+
+    // /api/v1/hello/** 식은 접두사 /api/v1/hello 가 붙은 모든 경로를 의미함
+    httpSecurity.authorizeHttpRequests(c -> c.requestMatchers("/api/v1/hello/**")
+        .authenticated());
+
+    // 나머지 요청은 모두 거부함
+    httpSecurity.authorizeHttpRequests(c -> c.anyRequest().denyAll());
+
+    httpSecurity.csrf(AbstractHttpConfigurer::disable);
+    
+    return httpSecurity.build();
+  }
 }
